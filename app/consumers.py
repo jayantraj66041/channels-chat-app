19ca9d1407396cc088b7570c9ec60601b8d16615
Jayant Raj <jayantraj66041@gmail.com>
2022-03-08 11:08:00 +0530

    adding enter event in chat.html page

--- app/consumers.py
+++ app/consumers.py
@@ -1,10 +1,11 @@
-from channels.generic.websocket import JsonWebsocketConsumer
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from app.models import Message
 from django.contrib.auth.models import User
-from asgiref.sync import async_to_sync
+# from asgiref.sync import async_to_sync
+from channels.db import database_sync_to_async
 
-class ChatAsyncWebsocketConsumer(JsonWebsocketConsumer):
-    def connect(self):
+class ChatAsyncWebsocketConsumer(AsyncJsonWebsocketConsumer):
+    async def connect(self):
         self.groupname = ""
         receiver = self.scope['url_route']['kwargs']['receiver']
         if str(self.scope['user']) > receiver:
@@ -13,37 +14,46 @@ class ChatAsyncWebsocketConsumer(JsonWebsocketConsumer):
             self.groupname = receiver + str(self.scope['user'])
         # print(self.groupname)
         # print(self.channel_name)
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.groupname, 
             self.channel_name
         )
 
-        self.accept()
+        await self.accept()
         # print("ok")
     
-    def receive_json(self, content, **kwargs):
+    async def receive_json(self, content, **kwargs):
         # print("first", content)
 
         content['sender'] = str(self.scope['user'])
 
-        message = Message()
-        message.msg = content['msg']
-        message.sender = User.objects.get(username=content['sender'])
-        message.receiver = User.objects.get(username=content['receiver'])
-        message.save()
+        if self.scope['user'].is_authenticated:
+            message = await database_sync_to_async(Message)()
+            message.msg = content['msg']
+            message.sender = await database_sync_to_async(User.objects.get)(username=content['sender'])
+            message.receiver = await database_sync_to_async(User.objects.get)(username=content['receiver'])
+            await database_sync_to_async(message.save)()
 
-        async_to_sync(self.channel_layer.group_send)(
-            self.groupname,
-            {
-                'type': 'chat.msg',
-                'message': content
-            }
-        )
+            await self.channel_layer.group_send(
+                self.groupname,
+                {
+                    'type': 'chat.msg',
+                    'message': content
+                }
+            )
+        else:
+            await self.send_json({
+                'message': "Login Required"
+            })
 
-    def chat_msg(self, event):
+    async def chat_msg(self, event):
         # event['message']['sender'] = str(self.scope['user'])
         # print("test",event)
-        self.send_json(event['message'])
+        await self.send_json(event['message'])
     
-    def disconnect(self, code):
+    async def disconnect(self, code):
         print("Disconnect -", code)
+        await self.channel_layer.group_discard(
+            self.groupname,
+            self.channel_name
+        )
